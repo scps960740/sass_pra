f28921e9402c5ca2a2ae57ffe15584bd47eb2f55
bruce.tseng <bruce.tseng@bytedance.com>
2021-07-31 18:29:58 +0800

    first commit
    Change-Id: I4eaeb8eccfc117e7ff58041174a8d4c329c9aecb

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import $ from 'jquery'
+import './index.css'
+import { a } from './a'
+
+// TODO del
+// console.log($.ajax)
+
+// TODO del
+console.log(a, PRODUCTION)
+
+class Test {
+  #a = 100
+} 
+
+const tt = new Test()
+// TODO del
+console.log('tt', tt.a)
